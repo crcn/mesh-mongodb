a66d662d7c0fd1f16ae0e41b7523b1d4928da4ed
Craig Condon <craig.condon@hellosign.com>
2015-06-19 18:20:28 -0700

    add test cases

--- test/mongodb-test.js
+++ test/mongodb-test.js
@@ -2,127 +2,23 @@ var mesh    = require("mesh");
 var mongodb = require("../");
 var _       = require("highland");
 var expect  = require("expect.js");
-var createTestCases = require("mesh-store-test-cases");
+var createTestCases = require("mesh/test-cases/database");
 
 describe(__filename + "#", function() {
 
-
-
-  // var cases = createTestCases(memoryDatabase);
-  // for (var name in cases) it(name, cases[name]);
-
   var db;
 
-  it("can be created", function(next) {
+  before(function() {
     db = mongodb("mongodb://localhost:27017/mesh-test");
-    db.adapter.once("connect", next);
   });
 
-  after(function() {
+  afterEach(function() {
     db.adapter.target.dropDatabase();
   });
 
-  it("returns an error if a collection is not provided", function(next) {
-    db(mesh.op("insert")).on("error", function(err) {
-      next();
-    });
+  var cases = createTestCases(function() {
+    return db;
   });
 
-  it("ignores operations that are not supported", function(next) {
-    db(mesh.op("afdfsfsd", { collection: "people", data: { name: "a"}})).on("end", function() {
-      next();
-    });
-  });
-
-  it("can insert an item", function(next) {
-    db(mesh.op("insert", { collection: "people", data: { name: "a"}})).on("data", function(data) {
-      expect(data._id).not.to.be(void 0);
-      expect(data.name).to.be("a");
-      next();
-    });
-  });
-
-  it("can insert multiple items", function(next) {
-    db(mesh.op("insert", {
-      collection: "people",
-      data: [{ name: "a" }, { name: "b" }]
-    })).
-    pipe(_.pipeline(_.collect)).
-    on("data", function(items) {
-      expect(items[0]._id).not.to.be(void 0);
-      expect(items[1]._id).not.to.be(void 0);
-      next();
-    });
-  });
-
-  describe("with data", function() {
-
-    var cdb;
-
-    beforeEach(function(next) {
-      cdb = mesh.child(db, { collection: "people" });
-      cdb(mesh.op("insert", {
-        data: [{ name: "a" }, { name: "b"}]
-      })).on("end", next);
-    });
-
-    it("can load one item", function(next) {
-      cdb(mesh.op("load", {
-        query: { name: "a" }
-      })).on("data", function(data) {
-        expect(data.name).to.be("a");
-        expect(data._id).not.to.be(void 0);
-        next();
-      });
-    });
-
-    it("can load multiple items", function(next) {
-      cdb(mesh.op("load", {
-        query: { name: /a|b/ },
-        multi: true
-      })).
-      pipe(_.pipeline(_.collect)).
-      on("data", function(items) {
-        expect(items.length).not.to.be(1);
-        expect(items[0].name).to.be("a");
-        next();
-      });
-    });
-
-    it("can remove an item", function(next) {
-      cdb(mesh.op("remove", {
-        query: { name: "a" }
-      })).on("end", function() {
-        next();
-      });
-    });
-
-    it("can remove many items item", function(next) {
-      cdb(mesh.op("remove", {
-        query: { name: "a" },
-        multi: true
-      })).on("end", function() {
-        next();
-      });
-    });
-
-    it("can update one item", function(next) {
-      cdb(mesh.op("update", {
-        query: { name: "a" },
-        data: { name: "b" }
-      })).on("end", function() {
-        next();
-      });
-    });
-
-    it("can update many items", function(next) {
-      cdb(mesh.op("update", {
-        query: { name: "a" },
-        data: { name: "b" },
-        multi: true
-      })).on("end", function() {
-        next();
-      });
-    });
-  });
+  for (var name in cases) it(name, cases[name]);
 });
